3a37c0ae88c6203dbf91cf459ce0a2af36dfbb5e
mauronofrio <poronofrio@gmail.com>
2020-06-24 16:25:25 +0000

    Rework prepdecrypt.sh
    Change-Id: Iebb5e85db4b8338244b3b837abd0263ff8e84fea

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -32,10 +32,10 @@ mount -t ext4 -o ro "$syspath" /s
 
 is_fastboot_twrp=$(getprop ro.boot.fastboot)
 if [ ! -z "$is_fastboot_twrp" ]; then
-	osver=$(getprop ro.build.version.release_orig)
-	patchlevel=$(getprop ro.build.version.security_patch_orig)
-	setprop ro.build.version.release "$osver"
-	setprop ro.build.version.security_patch "$patchlevel"
+	osver=$(getprop ro.build.version.release)
+	patchlevel=$(getprop ro.build.version.security_patch)
+	resetprop ro.build.version.release "$osver"
+	resetprop ro.build.version.security_patch "$patchlevel"
 	finish
 fi
 
@@ -43,15 +43,15 @@ if [ -f /s/system/build.prop ]; then
 	# TODO: It may be better to try to read these from the boot image than from /system
 	osver=$(grep -i 'ro.build.version.release' /s/system/build.prop  | cut -f2 -d'=')
 	patchlevel=$(grep -i 'ro.build.version.security_patch' /s/system/build.prop  | cut -f2 -d'=')
-	setprop ro.build.version.release "$osver"
-	setprop ro.build.version.security_patch "$patchlevel"
+	resetprop ro.build.version.release "$osver"
+	resetprop ro.build.version.security_patch "$patchlevel"
 	finish
 else
 	# Be sure to increase the PLATFORM_VERSION in build/core/version_defaults.mk to override Google's anti-rollback features to something rather insane
-	osver=$(getprop ro.build.version.release_orig)
-	patchlevel=$(getprop ro.build.version.security_patch_orig)
-	setprop ro.build.version.release "$osver"
-	setprop ro.build.version.security_patch "$patchlevel"
+	osver=$(getprop ro.build.version.release)
+	patchlevel=$(getprop ro.build.version.security_patch)
+	resetprop ro.build.version.release "$osver"
+	resetprop ro.build.version.security_patch "$patchlevel"
 	finish
 fi
 
